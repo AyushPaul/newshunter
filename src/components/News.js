f4731c0d822ed295e0c9fc524125fc356c6257ec
Ayush Paul <ayushpaul2002@gmail.com>
2022-09-12 15:06:45 +0530

    structured newsitems

--- src/components/News.js
+++ src/components/News.js
@@ -4,14 +4,41 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component.
-        <NewsItem title="mytitle" description="my description"/>
-        <NewsItem title="mytitle" description="my description"/>
-        <NewsItem title="mytitle" description="my description"/>
-        <NewsItem title="mytitle" description="my description"/>
-        <NewsItem title="mytitle" description="my description"/>
-        <NewsItem title="mytitle" description="my description"/>
+      <div className='container my-3'>
+        <h2>News Hunter - Top Headlines</h2>
+        <div className="row">
+            <div className="col-md-4 my-3">
+            <NewsItem title="mytitle" description="my description"/>
+            </div>
+            <div className="col-md-4 my-3">
+            <NewsItem title="mytitle" description="my description"/>
+            </div>
+            <div className="col-md-4 my-3">
+            <NewsItem title="mytitle" description="my description"/>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-md-4 my-3">
+            <NewsItem title="mytitle" description="my description"/>
+            </div>
+            <div className="col-md-4 my-3">
+            <NewsItem title="mytitle" description="my description"/>
+            </div>
+            <div className="col-md-4 my-3">
+            <NewsItem title="mytitle" description="my description"/>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-md-4 my-3">
+            <NewsItem title="mytitle" description="my description"/>
+            </div>
+            <div className="col-md-4 my-3">
+            <NewsItem title="mytitle" description="my description"/>
+            </div>
+            <div className="col-md-4 my-3">
+            <NewsItem title="mytitle" description="my description"/>
+            </div>
+        </div>
       </div>
 
     )

6bdb27385dd0f31f7c475754b04afd0b82e09ee8
Ayush Paul <ayushpaul2002@gmail.com>
2022-09-12 19:18:47 +0530

    implemented fetch api

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
     return (
       <div>
         <div className="card" style={{width: "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..."/>
+            <img src={!imageUrl?"https://fscluster.org/sites/default/files/styles/landscape-crop/public/default-image.png?itok=s9kBeTI4" : imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
